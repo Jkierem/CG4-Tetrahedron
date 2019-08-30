39185f269bb89692de4b23fa829b1742e67b38d9
Juan Miguel Gomez <juanmiguel.gomez@getbuilt.com>
2019-08-30 16:57:14 -0500

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,10 @@
+export const Colors = {
+    green: "#1c911c",
+    darkgreen: "#236e0c",
+    brown: "#965b03",
+    yellow: "#ffe51f",
+    blue: "#8dd7fc",
+    red: "#6e110c",
+    roof: "#6e4c0c",
+    black: "#000000"
+}
---
+++ src/index.js
@@ -0,0 +1,42 @@
+import * as p5 from 'p5';
+import { createCore } from './utils'
+import { Colors } from './data'
+
+const main = (p) => {
+    const core  = createCore(p);
+    const points = [
+        p.createVector(10,10,0),
+        p.createVector(-10,10,0),
+        p.createVector(-10,-10,0),
+        p.createVector(10,-10,0),
+        p.createVector(10,-10,10),
+        p.createVector(-10,-10,10),
+        p.createVector(-10,10,10),
+        p.createVector(10,10,10),
+    ]
+    p.setup = () => {
+        p.createCanvas(400,400,p.WEBGL);
+    };
+
+    p.draw = () => {
+        p.background(Colors.black);
+        core.open( (p) => {
+            const { width , height } = p;
+            p.translate( 0 , 40 , 0);
+            p.strokeWeight(3)
+            p.stroke(Colors.red);
+            p.fill(Colors.red);
+        }).safe((p,core) => {
+            points.forEach(
+                x => 
+                core.safe( () => {
+                    p.translate(...x.array())
+                    p.sphere(1)
+                })
+            )
+        })
+        .close()
+    };
+}
+
+const P5 = new p5(main);
