669ab4e0bad4898440bdd962a28d96e603faa2ba
Juan Miguel Gomez <juanmiguelgomez8@gmail.com>
2020-02-11 16:38:53 -0500

    unstable

--- src/index.js
+++ src/index.js
@@ -2,29 +2,31 @@ import * as p5 from 'p5';
 import { createCore } from './utils'
 import { Colors } from './data'
 
+const base = 40;
+
 const main = (p) => {
     const core  = createCore(p);
     const points = [
-        p.createVector(10,10,0),
-        p.createVector(-10,10,0),
-        p.createVector(-10,-10,0),
-        p.createVector(10,-10,0),
-        p.createVector(10,-10,10),
-        p.createVector(-10,-10,10),
-        p.createVector(-10,10,10),
-        p.createVector(10,10,10),
+        p.createVector(base,base,-base),
+        p.createVector(-base,base,-base),
+        p.createVector(-base,-base,-base),
+        p.createVector(base,-base,-base),
+        p.createVector(base,-base,base),
+        p.createVector(-base,-base,base),
+        p.createVector(-base,base,base),
+        p.createVector(base,base,base),
     ]
     p.setup = () => {
         p.createCanvas(400,400,p.WEBGL);
     };
-
+    let t = 0.001
+    const deltaT = 0.01;
     p.draw = () => {
         p.background(Colors.black);
+        t += deltaT;
         core.open( (p) => {
-            const { width , height } = p;
-            p.translate( 0 , 40 , 0);
-            p.strokeWeight(3)
-            p.rotateZ()
+            // p.translate( 0 , 0 , 0);
+            p.rotateY(t)
             p.stroke(Colors.red);
             p.fill(Colors.red);
         }).safe((p,core) => {
